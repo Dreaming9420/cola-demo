b8e022503fdc198d490a1804369d50edcf9aaf55
wangdaoren <wangdaoren@dangbei.com>
2024-04-25 17:10:10 +0800

    删除test

--- cola-demo-app/src/test/java/org/example/app/CustomerConvertorTest.java
+++
@@ -1,6 +0,0 @@
-package org.example.app;
-
-
-public class CustomerConvertorTest {
-
-}
--- cola-demo-infrastructure/src/test/java/org/example/repository/CustomerMapperTest.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-
-public class CustomerMapperTest {
-
-    public void testFindByID() {
-        System.out.println("Write your test here");
-    }
-}
